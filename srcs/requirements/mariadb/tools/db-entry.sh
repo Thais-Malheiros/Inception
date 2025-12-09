96481f5d944671f98aa0fdf66091913a00eb6b29
Thais Malheiros <thaismalheiros1983@gmail.com>
2025-12-09 13:27:15 -0300

    Update and expand documentation across the codebase

--- srcs/requirements/mariadb/tools/db-entry.sh
+++ srcs/requirements/mariadb/tools/db-entry.sh
@@ -13,6 +13,7 @@ mariadb -u root -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_
 # Grant full privileges on the specified database to the created user
 mariadb -u root -e "GRANT ALL ON $DB_NAME.* TO '$DB_USER'@'%';"
 
+# Applies all privilege changes to ensure newly created users and permissions take effect immediately
 mariadb -u root -e "FLUSH PRIVILEGES;"
 
 service mariadb stop
